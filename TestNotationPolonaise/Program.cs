cb7a6a992cbaa33236fd96808141949e276a9415
padrox <math.kessler@gmail.com>
2022-06-16 10:06:11 +0200

    ajout d'une fonction Polonaise

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -27,6 +27,59 @@ namespace TestNotationPolonaise
             } while (reponse != carac1 && reponse != carac2);
             return reponse;
         }
+        static double Polonaise(string formule)
+        {
+            //création du vecteur à partir de la chaine
+            String[] vec = formule.Split(' ');
+            // variable contenant la longueur de la chaine
+            int nbCase = formule.Length;
+            //boucle de lecture de la formule
+            while (nbCase>1)
+            {
+                //recherche de signe dans la formule
+                int k = nbCase - 1;
+                while (k > 0 && vec[k] != "+" && vec[k] != "-" && vec[k] != "/" && vec[k] != "*")
+                {
+                    k--;
+                }
+                // déclaration des membres de l'expression à calculer
+                double a = double.Parse(vec[k + 1]);
+                double b = double.Parse(vec[k + 2]);
+                double resultat = 0;
+                //calcul
+                switch (vec[k])
+                {
+                    case "+":
+                        resultat = a + b;
+                        break;
+                    case "-":
+                        resultat = a - b;
+                        break;
+                    case "*":
+                        resultat = a * b;
+                        break;
+                    case "/":
+                        if (b==0)
+                        {
+                            return double.NaN;
+                        }
+                        resultat = a / b;
+                        break;
+                }
+                // assigner le resultat dans la case du signe
+                vec[k] = resultat.ToString();
+                //déplacer les case vers la gauche
+                for (int j = k+1; j> nbCase-2;j++ )
+                {
+                    vec[j] = vec[j + 2];
+                }
+                //vider les 2 dernières cases
+                
+
+
+
+            }
+        }
 
         /// <summary>
         /// Saisie de formules en notation polonaise pour tester la fonction de calcul
